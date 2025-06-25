76622b2be6047153f0bdee5f08fd0b517c718d9c
Koeoe <161617683+betaRobinhood@users.noreply.github.com>
2025-06-25 16:08:25 +0700

    Update hello01.java
    change3

--- hello01.java
+++ hello01.java
@@ -1,5 +1,14 @@
 class Student{
+    private String name;
+    
     public Student(){
         //adgsajfjsadjfshfghsdfasfdasfdhsdafasdfasdfgsadf
+    }
+
+    private void setName(String name){
+        this.name = name;
+    }
+    private void getName(){
+        return name;
     }
 }
